3ab3ad7f0b0c61328219593972b9e53c7d292f8c
Liviu Ignat <liviu@ignat.email>
2015-11-23 22:04:33 +0100

    Add tests for languages

--- src/api/languages/__tests__/languages-test.js
+++ src/api/languages/__tests__/languages-test.js
@@ -12,7 +12,8 @@ describe('languageModule tests', () => {
         currentUser = user
       })
       .then(() => {
-        request.post('/api/projects')
+        request
+          .post('/api/projects')
           .set('Content-type', 'application/json')
           .set('Authorization', `Bearer ${currentUser.sessionToken}`)
           .send(currentProject)
@@ -46,6 +47,10 @@ describe('languageModule tests', () => {
     describe('When language request is finished', () => {
       let createdLanguage;
 
+      const getLanguageByIdRequest = () => request
+        .get(`/api/projects/${currentProject.objectId}/languages/${createdLanguage.objectId}`)
+        .set('Authorization', `Bearer ${currentUser.sessionToken}`);
+
       beforeEach((done) => {
         createLanguageRequest().end((err, response) => {
           if(err){
@@ -61,10 +66,38 @@ describe('languageModule tests', () => {
         expect(createdLanguage.key).to.equal(newLanguage.key);
       });
 
+      describe('When updating the language', () => {
+        let languageUpdate = {
+          key: "updated language",
+          name: "updated by me"
+        };
+
+        let updateLanguageRequest = (languageId) => request
+          .put(`/api/projects/${currentProject.objectId}/languages/${languageId}`)
+          .set('Authorization', `Bearer ${currentUser.sessionToken}`)
+          .set('Content-type', 'application/json')
+          .send(languageUpdate);
+
+        it('Should return success', (done) => {
+          updateLanguageRequest(createdLanguage.objectId).expect(200).end(done);
+        });
+
+        describe('When update request is finished', () => {
+          beforeEach((done) => updateLanguageRequest(createdLanguage.objectId).end(done));
+
+          it('Should have the language updated', (done) => {
+            getLanguageByIdRequest().end((err, res) => {
+              expect(res.body.key).to.equal(languageUpdate.key);
+              done(err);
+            });
+          })
+        });
+      });
+
       describe('When getting all languages', () => {
-         let getAllLanguagesRequest = () => request
-            .get(`/api/projects/${currentProject.objectId}/languages`)
-            .set('Authorization', `Bearer ${currentUser.sessionToken}`);
+        let getAllLanguagesRequest = () => request
+          .get(`/api/projects/${currentProject.objectId}/languages`)
+          .set('Authorization', `Bearer ${currentUser.sessionToken}`);
 
         it('Should return success', (done) => {
           getAllLanguagesRequest().expect(200).end(done);
@@ -81,10 +114,6 @@ describe('languageModule tests', () => {
         });
       });
       describe('When getting the language by id', () => {
-        const getLanguageByIdRequest = () => request
-            .get(`/api/projects/${currentProject.objectId}/languages/${createdLanguage.objectId}`)
-            .set('Authorization', `Bearer ${currentUser.sessionToken}`);
-
         it('Should return the language with success', (done) => {
            getLanguageByIdRequest().expect(200).end(done);
         });
