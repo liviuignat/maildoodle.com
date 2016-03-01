7a65e22d402d670cc5d929553d742b7bd33218f5
Liviu Ignat <liviu.ignat@appdirect.com>
2016-03-01 01:26:42 +0100

    Add boxes under second section

--- src/universal/containers/HomePage/HomePage.js
+++ src/universal/containers/HomePage/HomePage.js
@@ -1,9 +1,9 @@
 import React, {Component} from 'react';
 import {Link} from 'react-router';
-// import { RaisedButton } from './../../components';
+import HomePageCard from './HomePageCard';
 import config from './../../../config';
 
-export default class Home extends Component {
+export default class HomePage extends Component {
   render() {
     const style = require('./HomePage.scss');
     const appName = config.app.title;
@@ -26,7 +26,27 @@ export default class Home extends Component {
           </ul>
         </div>
 
-        <div className={style.HomePage_secondSection}>&nbsp;</div>
+        <div className={style.HomePage_secondSection}>
+          <div className={style.HomePage_cardContainer}>
+            <HomePageCard
+              imageSrc="http://getmdl.io/templates/android-dot-com/images/more-from-1.png"
+              titleText="Multiple templates"
+              bodyText="Easily create multiple html templates and edit them directly in the browser."
+              actionText="learn more >"/>
+
+            <HomePageCard
+              imageSrc="http://getmdl.io/templates/android-dot-com/images/more-from-4.png"
+              titleText="Different layouts"
+              bodyText="Create multiple page layouts and handle your campaign easily."
+              actionText="learn more >"/>
+
+            <HomePageCard
+              imageSrc="http://getmdl.io/templates/android-dot-com/images/more-from-3.png"
+              titleText="Test in the cloud"
+              bodyText="You can test your templates just with one click of the button."
+              actionText="learn more >"/>
+          </div>
+        </div>
       </div>
     );
   }
