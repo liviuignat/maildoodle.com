db90669b3a606b6bf3b6350ec22517556aa40dcb
Alina Staicu <alina.staicu4@gmail.com>
2016-03-14 20:31:32 +0100

    Fix google analytics and add user id correclty

--- src/universal/components/GoogleAnalytics/GoogleAnalytics.js
+++ src/universal/components/GoogleAnalytics/GoogleAnalytics.js
@@ -1,30 +1,6 @@
 import React, {Component, PropTypes} from 'react';
 import forOwn from 'lodash/forOwn';
 
-function initGoogleAnalytics(id, set = {}) {
-  if (window.ga || !id) {
-    return;
-  }
-
-  window.ga = window.ga || function(){(ga.q=ga.q||[]).push(arguments)};ga.l=+new Date; // eslint-disable-line
-
-  (function loadScript() {
-    const gads = document.createElement('script');
-    gads.async = true;
-    gads.type = 'text/javascript';
-    gads.src = '//www.google-analytics.com/analytics.js';
-
-    const head = document.getElementsByTagName('head')[0];
-    head.appendChild(gads);
-  })();
-
-  window.ga('create', id, 'auto');
-
-  forOwn(set, (value, key) => {
-    window.ga('set', key, value);
-  });
-}
-
 export default class GoogleAnalytics extends Component {
   static propTypes = {
     id: PropTypes.string,
@@ -37,12 +13,17 @@ export default class GoogleAnalytics extends Component {
   };
 
   componentDidMount() {
+    if (__DEVELOPMENT__) {
+      return;
+    }
+
     initGoogleAnalytics(this.props.id, this.props.set);
 
     this.historyListener = this.context.router.listen((location) => {
       if (!location) {
         return;
       }
+
       this.pageview(location);
     });
   }
@@ -68,38 +49,62 @@ export default class GoogleAnalytics extends Component {
 
     this.latestUrl = path;
     setTimeout(() => {
-      GoogleAnalytics.sendPageview(path, document.title, this.props.user);
+      sendPageview(path, document.title, this.props.user);
     }, 0);
-  }
-
-  static command(...args) {
-    if (!window.ga) {
-      throw new Error('Google analytics is not initialized');
-    }
-
-    return window.ga.apply(window.ga, args);
-  }
-
-  static send(what, options) {
-    return GoogleAnalytics.command('send', what, options);
-  }
-
-  static sendPageview(page, title = page, user = null) {
-    const payload = {page, title};
-    if (user) {
-      payload.user = {
-        id: user.objectId,
-        displayName: user.displayName
-      };
-    }
-    if (__DEVELOPMENT__) {
-      console.log('development analytics disabled: ', payload);
-      return null;
-    }
-    return GoogleAnalytics.send('pageview', payload);
   }
 
   render() {
     return (<span>Google Analytics</span>);
   }
+}
+
+function initGoogleAnalytics(id, sets = {}) {
+  if (window.ga || !id) {
+    return;
+  }
+
+  window.ga = window.ga || function(){(ga.q=ga.q||[]).push(arguments)};ga.l=+new Date; // eslint-disable-line
+
+  (function loadScript() {
+    const gads = document.createElement('script');
+    gads.async = true;
+    gads.type = 'text/javascript';
+    gads.src = '//www.google-analytics.com/analytics.js';
+
+    const head = document.getElementsByTagName('head')[0];
+    head.appendChild(gads);
+  })();
+
+  window.ga('create', id, 'auto');
+
+  forOwn(sets, (value, key) => {
+    set(key, value);
+  });
+}
+
+function command(...args) {
+  if (!window.ga) {
+    throw new Error('Google analytics is not initialized');
+  }
+
+  return window.ga.apply(window.ga, args);
+}
+
+function send(what, options) {
+  return command('send', what, options);
+}
+
+function set(key, value) {
+  return command('set', key, value);
+}
+
+function sendPageview(page, title = page, user = null) {
+  const payload = {page, title};
+
+  if (user) {
+    const userId = user.objectId;
+    set('userId', userId);
+  }
+
+  return send('pageview', payload);
 }
